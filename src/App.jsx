ff683e3fc209bfdcfe44aa23a059614821b63575
Muhammadfareed <ff063063063@gmail.com>
2025-01-16 01:33:41 +0500

    changes

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CardScanner from './pages/CardScanner'
+
+const App = () => {
+  return (
+    <div>
+      <CardScanner/>
+    </div>
+  )
+}
+
+export default App
